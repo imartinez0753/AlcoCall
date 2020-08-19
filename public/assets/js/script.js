4d3a1e653449b3fe3f16551f9bd7391310b443be
karazKOS <ykaraiscos@yahoo.com>
2020-08-18 18:38:37 -0700

    Materialize files updated

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,3 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 var instance = M.Carousel.init({
   fullWidth: true,
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".slider");
+  var instances = M.Slider.init(elems, options);
+});
