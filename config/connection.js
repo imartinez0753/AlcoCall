4bf3f008d9ff6dec6f237137c3c6e8d00faf2f8e
karazKOS <ykaraiscos@yahoo.com>
2020-08-20 21:13:58 -0700

    More changes

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Zugger9114",
-    database: "userInfo_db"
+    password: "Phoenix11!",
+    database: "userInfo_db",
   });
 }
 
